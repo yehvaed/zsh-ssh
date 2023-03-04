b7ac6a031e1dce595d7c61970c6b4ba4a13f2016
yehvaed <jaroslaw.walach@gmail.com>
2023-03-04 19:33:46 +0100

    refactor: autoload completion functions

--- zsh-ssh.plugin.zsh
+++ zsh-ssh.plugin.zsh
@@ -1,1 +1,32 @@
-zsh-ssh.zsh
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+
+# Return if requirements are missing
+if [[ $TERM == 'dumb' ]]; then
+  return 1
+fi
+
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Functions directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
+if [[ ${zsh_loaded_plugins[-1]} != */kalc && -z ${fpath[(r)${0:h}/functions]} ]]; then
+  fpath+=( "${0:h}/functions" )
+fi
+
+if [[ $PMSPEC != *f* ]]; then
+  fpath+=( "${0:h}/functions" )
+fi
+
+autoload -Uz fzf-complete-ssh
+
+[ -z "$fzf_ssh_default_completion" ] && {
+  binding=$(bindkey '^I')
+  [[ $binding =~ 'undefined-key' ]] || fzf_ssh_default_completion=$binding[(s: :w)2]
+  unset binding
+}
+
+zle -N fzf-complete-ssh
+bindkey '^I' fzf-complete-ssh
